9b53a7261ed2c0228b467577c9a7ea123a5d1517
The German <quincejozbruer@gmail.com>
2020-08-06 00:39:19 -0700

    Refractoring code, And start changing players gun

--- js/app.js
+++ js/app.js
@@ -1,20 +1,31 @@
 /*
-** Project Name: Board Game OPC Front-End Developer Path 
-** Author: Joz-Bruer Quince
-** Started Date: July 01 2020
-** Realease Date:
-** Licence: MIT
-*/
-import  {player1, player2}   from './player.class.js'
-import  {pistol,revolver,sniper,shotgun,assaultriffle} from "./gun.class.js"
+ ** Project Name: Board Game OPC Front-End Developer Path 
+ ** Author: Joz-Bruer Quince
+ ** Started Date: July 01 2020
+ ** Realease Date:
+ ** Licence: MIT
+ */
+import {
+    player1,
+    player2
+} from './player.class.js'
+import {
+    pistol,
+    revolver,
+    sniper,
+    shotgun,
+    assaultriffle
+} from "./gun.class.js"
 
 /*
-** Add data to the score board for the player one
-**
-*/
+ ** Add data to the score board for the player one
+ **
+ */
 
-// const player1SB = document.querySelectorAll('#player1')
-// let p = [player1.name,player1.gun['name'],player1.life, player1.gun['damage']]
+const player1SB = document.querySelectorAll('#player1')
+const player2SB = document.querySelectorAll('#player2')
+
+
 
 
 
@@ -26,11 +37,11 @@ const obstacleVarience = 0.12 //12 % of the map are obstacles
 
 
 /*
-** Create guns constant object
-** Create player constant object
-*/
-const players = [player1,player2]
-const guns = [pistol,sniper, revolver,shotgun,assaultriffle]
+ ** Create guns constant object
+ ** Create player constant object
+ */
+const players = [player1, player2]
+const guns = [pistol, sniper, revolver, shotgun, assaultriffle]
 
 
 let currentPlayer = players[0]
@@ -44,28 +55,44 @@ const maxMoves = 3
 let boardMap = []
 
 
-const addBox = (className) =>{
+const addBox = (className) => {
     let board = document.getElementById('board')
     let box = document.createElement('div')
-    box.setAttribute('class','box ' + className )
+    box.setAttribute('class', 'box ' + className)
     board.append(box)
 }
 
-const clearBoard = () =>{
-   document.getElementById('board').innerHTML = ""
+const clearBoard = () => {
+    document.getElementById('board').innerHTML = ""
 }
 
-const draw = () =>{
+const draw = () => {
     clearBoard()
-    for (let i = 0; i < rows; i++){
-        for (let j = 0; j < cols; j++){
-            addBox(boardMap[i][j])              
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            addBox(boardMap[i][j])
         }
-        
+
     }
+    let player1arr = [players[0].name, players[0].gun['name'], players[0].life, players[0].gun['damage']]
+    let player2arr = [players[1].name, players[1].gun['name'], players[1].life, players[1].gun['damage']]
+    let i = 0
+    let j = 0
+
+
+    player1SB.forEach(element => {
+        element.innerText = player1arr[i]
+        i++
+    });
+
+
+    player2SB.forEach(element => {
+        element.innerText = player2arr[j]
+        j++
+    });
 }
 
-const generateBoard = () =>{
+const generateBoard = () => {
     boardMap = []
     addFreeSpaces()
     addObstacles()
@@ -74,107 +101,133 @@ const generateBoard = () =>{
 }
 
 /* Reset boardmap */
-const addFreeSpaces = () =>{
-    for (let i = 0; i < rows; i++){
+const addFreeSpaces = () => {
+    for (let i = 0; i < rows; i++) {
         boardMap.push([])
-        for (let j = 0; j < cols; j++){  
-            boardMap[i][j] = 'free'            
+        for (let j = 0; j < cols; j++) {
+            boardMap[i][j] = 'free'
         }
     }
 
 }
 
-const addObstacles = () =>{
+const addObstacles = () => {
     let i = 0
     const numOfObstacles = obstacleVarience * rows * cols
     let randomI
     let randomJ
 
-    while (i < numOfObstacles  ){
+    while (i < numOfObstacles) {
         randomI = Math.floor(Math.random() * rows)
         randomJ = Math.floor(Math.random() * cols)
-        if (boardMap[randomI][randomJ] === 'free'){
+        if (boardMap[randomI][randomJ] === 'free') {
             boardMap[randomI][randomJ] = 'obstacle'
             i++
         }
     }
 }
 
-const addPlayers = () =>{
+const addPlayers = () => {
 
     let i = 0
     let randomI
     let randomJ
-    while (i < players.length  ){
+    while (i < players.length) {
         randomI = Math.floor(Math.random() * rows)
         randomJ = Math.floor(Math.random() * cols)
-        if (boardMap[randomI][randomJ] === 'free'){
+        if (boardMap[randomI][randomJ] === 'free') {
             boardMap[randomI][randomJ] = players[i].name
-            players[i].setPosition(randomI,randomJ)
+            players[i].setPosition(randomI, randomJ)
             i++
         }
     }
 }
 
 
-const addGuns = () =>{
+const addGuns = () => {
 
     let i = 0
     let randomI
     let randomJ
-    while (i < guns.length  ){
+    while (i < guns.length) {
         randomI = Math.floor(Math.random() * rows)
         randomJ = Math.floor(Math.random() * cols)
-        if (boardMap[randomI][randomJ] === 'free'){
+        if (boardMap[randomI][randomJ] === 'free') {
             boardMap[randomI][randomJ] = guns[i].name
             i++
         }
     }
 }
 
-const move = (newX, newY) =>{
+const move = (newX, newY) => {
     // Check if newX and newY are within 0 to rows and 0 to cols
-    if (newX >= 0 && newX < rows && newY >= 0 && newY < cols){
-        if (boardMap[newX][newY] === 'free'){
+    if (newX >= 0 && newX < rows && newY >= 0 && newY < cols) {
+        if (boardMap[newX][newY] === 'free') {
             let [oldX, oldY] = [currentPlayer.x, currentPlayer.y]
             boardMap[oldX][oldY] = 'free'
             boardMap[newX][newY] = currentPlayer.name
-            currentPlayer.setPosition(newX,newY)
+            currentPlayer.setPosition(newX, newY)
             draw()
-            currentMoves++
-            checkMaxMoves()
+
+
+        } else if (boardMap[newX][newY] === 'pistol' || boardMap[newX][newY] === 'sniper' || boardMap[newX][newY] === 'revolver' ||
+            boardMap[newX][newY] === 'assaultriffle' || boardMap[newX][newY] === 'shotgun') {
+            // Check the players gun
+            let newGun = boardMap[newX][newY]
+            if (currentPlayer.gun['name'] === 'knife') {
+                let [oldX, oldY] = [currentPlayer.x, currentPlayer.y]
+                boardMap[oldX][oldY] = 'free'
+                boardMap[newX][newY] = currentPlayer.name
+                currentPlayer.setPosition(newX, newY)
+                currentPlayer.gun['name'] = newGun
+                draw()
+
+
+                // console.log(newGun)
+                // break
+            }
         }
+
+        currentMoves++
+        checkMaxMoves()
     }
-    
+
 }
 
-const checkMaxMoves = () =>{
-    if (currentMoves >=  maxMoves){
+const checkMaxMoves = () => {
+    if (currentMoves >= maxMoves) {
         switchPlayer()
     }
-} 
+}
 
 const switchPlayer = () => {
-    if (currentPlayer.name == 'player1'){
+    if (currentPlayer.name == 'player1') {
         currentPlayer = players[1]
-    }
-    else{
+    } else {
         currentPlayer = players[0]
     }
     currentMoves = 0
 }
+
+// const pickupNewGun = () =>{
+//     if (currentPlayer.gun['name'] === 'knife'){
+//         console.log('Knife is the gun', currentPlayer.name)
+//     }
+// }
+
+
 const handleKey = (e) => {
     e.preventDefault()
-    
-    switch (e.which){
-        case 37: 
-        move(currentPlayer.x, currentPlayer.y - 1)
-        break
+
+    switch (e.which) {
+        case 37:
+            move(currentPlayer.x, currentPlayer.y - 1)
+            break
         case 38:
             move(currentPlayer.x - 1, currentPlayer.y)
             break
         case 39:
-            move(currentPlayer.x , currentPlayer.y + 1)
+            move(currentPlayer.x, currentPlayer.y + 1)
             break
         case 40:
             move(currentPlayer.x + 1, currentPlayer.y)
@@ -182,7 +235,7 @@ const handleKey = (e) => {
     }
 }
 
- generateBoard()
- draw()
- 
- document.addEventListener('keydown', handleKey)
+generateBoard()
+draw()
+
+document.addEventListener('keydown', handleKey)
