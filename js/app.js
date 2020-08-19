ce2aa545d8932da8f8a831741bbf0e67c5dbe332
The German <quincejozbruer@gmail.com>
2020-08-19 13:33:58 -0700

    Create a scoreboard function

--- js/app.js
+++ js/app.js
@@ -5,16 +5,13 @@
  ** Realease Date:
  ** Licence: MIT
  */
+import { player1, player2 } from "./player.class.js";
 import {
-    player1,
-    player2
-} from "./player.class.js";
-import {
-    pistol,
-    revolver,
-    sniper,
-    shotgun,
-    assaultriffle,
+  pistol,
+  revolver,
+  sniper,
+  shotgun,
+  assaultriffle,
 } from "./gun.class.js";
 
 /*
@@ -22,15 +19,13 @@ import {
  **
  */
 
+const scoreBoard = document.querySelectorAll(".score-board-text");
+scoreBoard[0].classList.add("selected");
 
-
-const scoreBoard = document.querySelectorAll('.score-board-text')
-scoreBoard[0].classList.add('selected')
-
-const player1SB = document.querySelectorAll("#player1")
-const player2SB = document.querySelectorAll("#player2")
-const player1Modal = document.querySelector("#player1Modal")
-const player2Modal = document.querySelector("#player2Modal")
+const player1SB = document.querySelectorAll("#player1");
+const player2SB = document.querySelectorAll("#player2");
+const player1Modal = document.querySelector("#player1Modal");
+const player2Modal = document.querySelector("#player2Modal");
 
 const rows = 10;
 const cols = 10;
@@ -49,268 +44,270 @@ let currentMoves = 0;
 const maxMoves = 3;
 
 const resultBoard = () => {
-    let player1arr = [
-        players[0].name,
-        players[0].gun.name,
-        players[0].life,
-        players[0].gun.damage,
-    ];
-    let player2arr = [
-        players[1].name,
-        players[1].gun.name,
-        players[1].life,
-        players[1].gun.damage,
-    ];
-    let i = 0;
-    let j = 0;
+  let player1arr = [
+    players[0].name,
+    players[0].gun.name,
+    players[0].life,
+    players[0].gun.damage,
+  ];
+  let player2arr = [
+    players[1].name,
+    players[1].gun.name,
+    players[1].life,
+    players[1].gun.damage,
+  ];
+  let i = 0;
+  let j = 0;
 
-    player1SB.forEach(element => {
-        element.innerText = player1arr[i];
-        i++;
-    });
+  player1SB.forEach((element) => {
+    element.innerText = player1arr[i];
+    i++;
+  });
 
-    player2SB.forEach(element => {
-        element.innerText = player2arr[j];
-        j++;
-    });
-    player1Modal.innerText = players[0].gun.name
-    player2Modal.innerText = players[1].gun.name
+  player2SB.forEach((element) => {
+    element.innerText = player2arr[j];
+    j++;
+  });
+  player1Modal.innerText = players[0].gun.name;
+  player2Modal.innerText = players[1].gun.name;
 };
 
 let boardMap = [];
 
 const addBox = (className) => {
-    let board = document.getElementById("board");
-    let box = document.createElement("div");
-    box.setAttribute("class", "box " + className);
-    board.append(box);
+  let board = document.getElementById("board");
+  let box = document.createElement("div");
+  box.setAttribute("class", "box " + className);
+  board.append(box);
 };
 
 const clearBoard = () => {
-    document.getElementById("board").innerHTML = "";
+  document.getElementById("board").innerHTML = "";
 };
 
 const draw = () => {
-    clearBoard();
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            addBox(boardMap[i][j]);
-        }
+  clearBoard();
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      addBox(boardMap[i][j]);
     }
-    resultBoard();
-
+  }
+  resultBoard();
+  updateSwitchBorder();
 };
 
 const generateBoard = () => {
-    boardMap = [];
-    addFreeSpaces();
-    addObstacles();
-    addPlayers();
-    addGuns(guns);
+  boardMap = [];
+  addFreeSpaces();
+  addObstacles();
+  addPlayers();
+  addGuns(guns);
 };
 
 /* Reset boardmap */
 const addFreeSpaces = () => {
-    for (let i = 0; i < rows; i++) {
-        boardMap.push([]);
-        for (let j = 0; j < cols; j++) {
-            boardMap[i][j] = "free";
-        }
+  for (let i = 0; i < rows; i++) {
+    boardMap.push([]);
+    for (let j = 0; j < cols; j++) {
+      boardMap[i][j] = "free";
     }
+  }
 };
 
 const addObstacles = () => {
-    let i = 0;
-    const numOfObstacles = obstacleVarience * rows * cols;
-    let randomI;
-    let randomJ;
+  let i = 0;
+  const numOfObstacles = obstacleVarience * rows * cols;
+  let randomI;
+  let randomJ;
 
-    while (i < numOfObstacles) {
-        randomI = Math.floor(Math.random() * rows);
-        randomJ = Math.floor(Math.random() * cols);
-        if (boardMap[randomI][randomJ] === "free") {
-            boardMap[randomI][randomJ] = "obstacle";
-            i++;
-        }
+  while (i < numOfObstacles) {
+    randomI = Math.floor(Math.random() * rows);
+    randomJ = Math.floor(Math.random() * cols);
+    if (boardMap[randomI][randomJ] === "free") {
+      boardMap[randomI][randomJ] = "obstacle";
+      i++;
     }
+  }
 };
 
 const addPlayers = () => {
-    let i = 0;
-    let randomI;
-    let randomJ;
-    while (i < players.length) {
-        randomI = Math.floor(Math.random() * rows);
-        randomJ = Math.floor(Math.random() * cols);
-        if (boardMap[randomI][randomJ] === "free") {
-            boardMap[randomI][randomJ] = players[i].name;
-            players[i].setPosition(randomI, randomJ);
-            i++;
-        }
+  let i = 0;
+  let randomI;
+  let randomJ;
+  while (i < players.length) {
+    randomI = Math.floor(Math.random() * rows);
+    randomJ = Math.floor(Math.random() * cols);
+    if (boardMap[randomI][randomJ] === "free") {
+      boardMap[randomI][randomJ] = players[i].name;
+      players[i].setPosition(randomI, randomJ);
+      i++;
     }
+  }
 };
 
 const addGuns = () => {
-    let i = 0;
-    let randomI;
-    let randomJ;
-    while (i < guns.length) {
-        randomI = Math.floor(Math.random() * rows);
-        randomJ = Math.floor(Math.random() * cols);
-        if (boardMap[randomI][randomJ] === "free") {
-            boardMap[randomI][randomJ] = guns[i].name;
-            i++;
-        }
+  let i = 0;
+  let randomI;
+  let randomJ;
+  while (i < guns.length) {
+    randomI = Math.floor(Math.random() * rows);
+    randomJ = Math.floor(Math.random() * cols);
+    if (boardMap[randomI][randomJ] === "free") {
+      boardMap[randomI][randomJ] = guns[i].name;
+      i++;
     }
+  }
 };
 
-
 const move = (newX, newY) => {
-    // Check if newX and newY are within 0 to rows and 0 to cols
-    if (newX >= 0 && newX < rows && newY >= 0 && newY < cols) {
-        if (boardMap[newX][newY] === "free") {
-            let [oldX, oldY] = [currentPlayer.x, currentPlayer.y];
-            boardMap[oldX][oldY] = "free";
-            boardMap[newX][newY] = currentPlayer.name;
-            currentPlayer.setPosition(newX, newY);
-            draw();
-            if (checkBattleMode() == true) {
-                showBattleScreen()
-            } else {
-                currentMoves++;
-                checkMaxMoves();
-            }
-
-        } else if (
-            boardMap[newX][newY] === "pistol" ||
-            boardMap[newX][newY] === "sniper" ||
-            boardMap[newX][newY] === "revolver" ||
-            boardMap[newX][newY] === "assaultriffle" ||
-            boardMap[newX][newY] === "shotgun"
-        ) {
-            // Check the players gun
-            let newGunName = boardMap[newX][newY];
-            let newGun = guns.filter(gun => gun.name === newGunName)[0]
-            let [oldX, oldY] = [currentPlayer.x, currentPlayer.y];
-            if (currentPlayer.gun.name === "knife") {
-                boardMap[oldX][oldY] = "free";
-            } else {
-                let oldGun = currentPlayer.gun;
-                boardMap[oldX][oldY] = oldGun.name
-            }
-            boardMap[newX][newY] = currentPlayer.name;
-            currentPlayer.setPosition(newX, newY);
-            currentPlayer.gun = newGun;
-            draw();
-            if (checkBattleMode() == true) {
-                showBattleScreen()
-            } else {
-                currentMoves++;
-                checkMaxMoves();
-            }
-        }
-        // checkBattleCondition()
+  // Check if newX and newY are within 0 to rows and 0 to cols
+  if (newX >= 0 && newX < rows && newY >= 0 && newY < cols) {
+    if (boardMap[newX][newY] === "free") {
+      let [oldX, oldY] = [currentPlayer.x, currentPlayer.y];
+      boardMap[oldX][oldY] = "free";
+      boardMap[newX][newY] = currentPlayer.name;
+      currentPlayer.setPosition(newX, newY);
+      draw();
+      if (checkBattleMode() == true) {
+        showBattleScreen();
+      } else {
+        currentMoves++;
+        checkMaxMoves();
+      }
+    } else if (
+      boardMap[newX][newY] === "pistol" ||
+      boardMap[newX][newY] === "sniper" ||
+      boardMap[newX][newY] === "revolver" ||
+      boardMap[newX][newY] === "assaultriffle" ||
+      boardMap[newX][newY] === "shotgun"
+    ) {
+      // Check the players gun
+      let newGunName = boardMap[newX][newY];
+      let newGun = guns.filter((gun) => gun.name === newGunName)[0];
+      let [oldX, oldY] = [currentPlayer.x, currentPlayer.y];
+      if (currentPlayer.gun.name === "knife") {
+        boardMap[oldX][oldY] = "free";
+      } else {
+        let oldGun = currentPlayer.gun;
+        boardMap[oldX][oldY] = oldGun.name;
+      }
+      boardMap[newX][newY] = currentPlayer.name;
+      currentPlayer.setPosition(newX, newY);
+      currentPlayer.gun = newGun;
+      draw();
+      if (checkBattleMode() == true) {
+        showBattleScreen();
+      } else {
+        currentMoves++;
+        checkMaxMoves();
+      }
     }
+    // checkBattleCondition()
+  }
 };
 
 const checkMaxMoves = () => {
-    if (currentMoves >= maxMoves) {
-        switchPlayer();
-    }
+  if (currentMoves >= maxMoves) {
+    switchPlayer();
+  }
 };
-
+const updateSwitchBorder = () => {
+  if (currentPlayer.name === "player2") {
+    scoreBoard[0].classList.remove("selected");
+    scoreBoard[1].classList.add("selected");
+  } else {
+    scoreBoard[0].classList.add("selected");
+    scoreBoard[1].classList.remove("selected");
+  }
+};
 const switchPlayer = () => {
-    if (currentPlayer.name === "player1") {
-        currentPlayer = players[1]
-        scoreBoard[0].classList.remove('selected')
-        scoreBoard[1].classList.add('selected')
-
-    } else {
-        currentPlayer = players[0]
-        scoreBoard[0].classList.add('selected')
-        scoreBoard[1].classList.remove('selected')
-    }
-    currentMoves = 0;
+  if (currentPlayer.name === "player1") {
+    currentPlayer = players[1];
+  } else {
+    currentPlayer = players[0];
+  }
+  //   updateSwitchBorder();
+  currentMoves = 0;
 };
 
 const checkBattleMode = () => {
-    let nextPlayer = getOtherPlayer()
+  let nextPlayer = getOtherPlayer();
 
-    if (
-        boardMap[currentPlayer.x + 1][currentPlayer.y] === nextPlayer.name ||
-        boardMap[currentPlayer.x - 1][currentPlayer.y] === nextPlayer.name ||
-        boardMap[currentPlayer.x][currentPlayer.y + 1] === nextPlayer.name ||
-        boardMap[currentPlayer.x][currentPlayer.y - 1] === nextPlayer.name) {
+  if (
+    boardMap[currentPlayer.x + 1][currentPlayer.y] === nextPlayer.name ||
+    boardMap[currentPlayer.x - 1][currentPlayer.y] === nextPlayer.name ||
+    boardMap[currentPlayer.x][currentPlayer.y + 1] === nextPlayer.name ||
+    boardMap[currentPlayer.x][currentPlayer.y - 1] === nextPlayer.name
+  ) {
+    //  disable the keypress event
+    // modalContainer.classList.add('show')
+    return true;
+  }
 
-        //  disable the keypress event 
-        // modalContainer.classList.add('show')
-        return true
-    }
-
-    return false
-}
-
+  return false;
+};
 
 const showBattleScreen = () => {
-    //disable key press event
-    enableDisableButtons()
-    modalContainer.classList.add('show')
-}
+  //disable key press event
+  //   document.addEventListener("keydown", disableKeyPress);
+  enableDisableButtons();
+  modalContainer.classList.add("show");
+};
 const getOtherPlayer = () => {
-    if (currentPlayer.name === "player1")
-        return players[1]
-    else
-        return players[0]
-}
+  if (currentPlayer.name === "player1") return players[1];
+  else return players[0];
+};
 const enableDisableButtons = () => {
-    if (currentPlayer.name === 'player1') {
-        document.getElementById('attack1').setAttribute('disabled', false)
-        document.getElementById('attack2').setAttribute('disabled', true)
-        document.getElementById('deffence1').setAttribute('disabled', false)
-        document.getElementById('deffence2').setAttribute('disabled', true)
-    } else {
-        document.getElementById('attack2').setAttribute('disabled', false)
-        document.getElementById('attack1').setAttribute('disabled', true)
-        document.getElementById('deffence2').setAttribute('disabled', false)
-        document.getElementById('deffence1').setAttribute('disabled', true)
-    }
-}
+  if (currentPlayer.name === "player1") {
+    document.getElementById("attack1").setAttribute("disabled", false);
+    document.getElementById("attack2").setAttribute("disabled", true);
+    document.getElementById("deffence1").setAttribute("disabled", false);
+    document.getElementById("deffence2").setAttribute("disabled", true);
+  } else {
+    document.getElementById("attack2").setAttribute("disabled", false);
+    document.getElementById("attack1").setAttribute("disabled", true);
+    document.getElementById("deffence2").setAttribute("disabled", false);
+    document.getElementById("deffence1").setAttribute("disabled", true);
+  }
+};
 
 const attack = () => {
-    let otherPlayer = getOtherPlayer()
-    otherPlayer.takeLife(currentPlayer.gun.damage)
-    if (otherPlayer.alive) {
-        switchPlayer()
-        enableDisableButtons()
-    } else {
-        // Hide battle modal, show the game over modal
-        console.log('Game over')
-    }
-}
+  let otherPlayer = getOtherPlayer();
+  otherPlayer.takeLife(currentPlayer.gun.damage);
+  if (otherPlayer.alive) {
+    switchPlayer();
+    enableDisableButtons();
+  } else {
+    // Hide battle modal, show the game over modal
+    console.log("Game over");
+  }
+};
 
 const defend = () => {
-    currentPlayer.defence = true
-    switchPlayer()
-    enableDisableButtons()
-}
-
+  currentPlayer.defence = true;
+  switchPlayer();
+  enableDisableButtons();
+};
+const disableKeyPress = (e) => {
+  e.preventDefault();
+};
 const handleKey = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    switch (e.which) {
-        case 37:
-            move(currentPlayer.x, currentPlayer.y - 1);
-            break;
-        case 38:
-            move(currentPlayer.x - 1, currentPlayer.y);
-            break;
-        case 39:
-            move(currentPlayer.x, currentPlayer.y + 1);
-            break;
-        case 40:
-            move(currentPlayer.x + 1, currentPlayer.y);
-            break;
-    }
+  switch (e.which) {
+    case 37:
+      move(currentPlayer.x, currentPlayer.y - 1);
+      break;
+    case 38:
+      move(currentPlayer.x - 1, currentPlayer.y);
+      break;
+    case 39:
+      move(currentPlayer.x, currentPlayer.y + 1);
+      break;
+    case 40:
+      move(currentPlayer.x + 1, currentPlayer.y);
+      break;
+  }
 };
 
 generateBoard();
@@ -318,20 +315,17 @@ draw();
 
 document.addEventListener("keydown", handleKey);
 
-
-
 /**
  * Modal events
  */
 
-const open = document.getElementById('open')
-const modalContainer = document.getElementById('modal_container')
-const close = document.getElementById('close')
-
+const open = document.getElementById("open");
+const modalContainer = document.getElementById("modal_container");
+const close = document.getElementById("close");
 
 // open.addEventListener('click', () => {
 //     modalContainer.classList.add('show')
 // })
-close.addEventListener('click', () => {
-    modalContainer.classList.remove('show')
-})
+close.addEventListener("click", () => {
+  modalContainer.classList.remove("show");
+});
